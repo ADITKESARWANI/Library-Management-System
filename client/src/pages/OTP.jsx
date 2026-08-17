64708cabf732b7392afc565e1b5db6cf5c9aa144
Aditya Kesarwani <kesarwaniaditya599@gmail.com>
2026-08-17 16:31:22 +0530

    Fix logo import and update OTP component

--- client/src/pages/OTP.jsx
+++ client/src/pages/OTP.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import logo from "../assets/logo-with-title-black-55GCaCS6.png";
-import logo from "../assets/logo-with-title-D7a_5BuJ.png";
+import logo_with_title from "../assets/logo-with-title-D7a_5BuJ.png";
 import { Link, useParams, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { otpVerification, resetAuthSlice } from "../store/slices/authSlice";
